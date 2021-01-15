7be975d1fc673ac835c5abff3b664b11e258fd53
Yuntaek <yuntaek.lim@wavve.com>
2021-01-15 15:50:54 +0900

    fixed  pipe issus add Formater for srt ass tag

--- src/Formatter.ts
+++ src/Formatter.ts
@@ -33,15 +33,66 @@ export class Formatter {
     return buffer
   }
 
+  private formatCueForSrtWithassTag(cue: Cue): Cue {
+    let assTag: string = ''
+    if (cue.settings) {
+      const rawPosition = cue.settings.match(/position\s?=\s?([0-9]+)%/)
+      const align = cue.settings.match(/align\s?=\s?([a-z]+)/)
+      if (rawPosition && align) {
+        const position = parseInt(rawPosition[1])
+        if (position >= 90) {
+          switch (align[1]) {
+            case 'start ':
+              assTag = 'an7'
+              break
+            case 'center':
+              assTag = 'an8'
+              break
+            case 'end':
+              assTag = 'an9'
+              break
+          }
+        } else if (position <= 10) {
+          switch (align[0]) {
+            case 'start ':
+              assTag = 'an1'
+              break
+            case 'center':
+              assTag = 'an2'
+              break
+            case 'end':
+              assTag = 'an3'
+              break
+          }
+        } else {
+          switch (align[0]) {
+            case 'start ':
+              assTag = 'an4'
+              break
+            case 'center':
+              assTag = 'an5'
+              break
+            case 'end':
+              assTag = 'an6'
+              break
+          }
+        }
+        cue.text = assTag !== '' ? `{\\${assTag}}${cue.text}` : cue.text
+      }
+    }
+    return cue
+  }
   private formatCue(cue: Cue, index: number, options: FormatOptions) {
+    if (options.format === 'SRT') {
+      cue = this.formatCueForSrtWithassTag(cue)
+    }
     return [
       `${index > 1 ? '\n' : ''}${index}`,
       `${formatTimestamp(cue.start, options)} --> ${formatTimestamp(
         cue.end,
         options
-      )}${
-        options.format === 'WebVTT' && cue.settings ? ' ' + cue.settings : ''
-      }`,
+      )}${options.format ===
+        ('WebVTT' && cue.settings ? ' ' + cue.settings : '')}`,
       cue.text,
       ''
     ].join('\n')
--- src/filter.ts
+++ src/filter.ts
@@ -1,12 +1,15 @@
 import { Node } from '.'
-import { createDuplex } from './utils'
+import { Transform } from 'stream'
 
-export const filter = (callback: (node: Node) => boolean) =>
-  createDuplex({
-    write(chunk, _encoding, next) {
+export const filter = (callback: (node: Node) => boolean) => {
+  return new Transform({
+    objectMode: true,
+    autoDestroy: false,
+    transform(chunk: Node, _encoding, next) {
       if (callback(chunk)) {
         this.push(chunk)
       }
       next()
     }
   })
+}
